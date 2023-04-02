262584746cb6664b2263845c87c679cf046a0a28
Simon Gauvin <gauvinsimon@gmail.com>
2023-04-02 12:31:20 -0400

    Still working

--- crates/app/src/renderer.rs
+++ crates/app/src/renderer.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+use std::num::NonZeroU8;
 use std::{num::NonZeroU64, sync::Arc};
 
 use egui_wgpu::wgpu::util::DeviceExt;
@@ -13,8 +15,12 @@ impl Custom3d {
         // from `eframe::Frame` when you don't have a `CreationContext` available.
         let render_state = cc.wgpu_render_state.as_ref()?;
 
-        let patnais = Self::create_screen_pipeline(&render_state);
-
+        let raytracing_resources = Self::create_raytracing_pipeline(render_state);
+        let triangle_resources = Self::create_screen_pipeline(render_state);
+        let resources = Resources {
+            raytracing_resources,
+            triangle_resources,
+        };
         // Because the graphics pipeline must have the same lifetime as the egui render pass,
         // instead of storing the pipeline in our `Custom3D` struct, we insert it into the
         // `paint_callback_resources` type map, which is stored alongside the render pass.
@@ -22,9 +28,93 @@ impl Custom3d {
             .renderer
             .write()
             .paint_callback_resources
-            .insert(patnais);
+            .insert(resources);
 
         Some(Self { angle: 0.0 })
+    }
+
+    fn create_raytracing_pipeline(
+        render_state: &egui_wgpu::RenderState,
+    ) -> RaytracingRenderResources {
+        let device = &render_state.device;
+
+        let texture_descriptor = wgpu::TextureDescriptor {
+            size: wgpu::Extent3d {
+                width: 300,
+                height: 300,
+                depth_or_array_layers: 1,
+            },
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: wgpu::TextureFormat::Rgba8Unorm,
+            usage: wgpu::TextureUsages::STORAGE_BINDING
+                | wgpu::TextureUsages::RENDER_ATTACHMENT
+                | wgpu::TextureUsages::COPY_SRC,
+            label: None,
+            view_formats: &[],
+        };
+        let color_buffer = device.create_texture(&texture_descriptor);
+        let color_buffer_view = color_buffer.create_view(&wgpu::TextureViewDescriptor::default());
+
+        let sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+            address_mode_u: wgpu::AddressMode::Repeat,
+            address_mode_v: wgpu::AddressMode::Repeat,
+            address_mode_w: wgpu::AddressMode::Repeat,
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Nearest,
+            mipmap_filter: wgpu::FilterMode::Nearest,
+            anisotropy_clamp: std::num::NonZeroU8::new(1),
+            ..Default::default()
+        });
+
+        let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::StorageTexture {
+                    access: wgpu::StorageTextureAccess::WriteOnly,
+                    format: wgpu::TextureFormat::Rgba8Unorm,
+                    view_dimension: wgpu::TextureViewDimension::D2,
+                },
+                count: None,
+            }],
+            label: None,
+        });
+
+        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &bind_group_layout,
+            label: None,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::TextureView(&color_buffer_view),
+            }],
+        });
+
+        let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            bind_group_layouts: &[&bind_group_layout],
+            ..Default::default()
+        });
+
+        let cs_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: None,
+            source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!(
+                "./shaders/raytracer_kernel.wgsl"
+            ))),
+        });
+
+        let pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+            label: None,
+            layout: Some(&pipeline_layout),
+            module: &cs_module,
+            entry_point: "main",
+        });
+
+        RaytracingRenderResources {
+            bind_group,
+            pipeline,
+            sampler,
+        }
     }
 
     fn create_screen_pipeline(render_state: &egui_wgpu::RenderState) -> TriangleRenderResources {
@@ -125,12 +215,12 @@ impl Custom3d {
         // can be used to issue draw commands.
         let cb = egui_wgpu::CallbackFn::new()
             .prepare(move |device, queue, _encoder, paint_callback_resources| {
-                let resources: &TriangleRenderResources = paint_callback_resources.get().unwrap();
+                let resources: &Resources = paint_callback_resources.get().unwrap();
                 resources.prepare(device, queue, angle);
                 Vec::new()
             })
             .paint(move |_info, render_pass, paint_callback_resources| {
-                let resources: &TriangleRenderResources = paint_callback_resources.get().unwrap();
+                let resources: &Resources = paint_callback_resources.get().unwrap();
                 resources.paint(render_pass);
             });
 
@@ -147,6 +237,27 @@ struct TriangleRenderResources {
     pipeline: wgpu::RenderPipeline,
     bind_group: wgpu::BindGroup,
     uniform_buffer: wgpu::Buffer,
+}
+
+struct RaytracingRenderResources {
+    pipeline: wgpu::ComputePipeline,
+    sampler: wgpu::Sampler,
+    bind_group: wgpu::BindGroup,
+}
+
+struct Resources {
+    raytracing_resources: RaytracingRenderResources,
+    triangle_resources: TriangleRenderResources,
+}
+
+impl Resources {
+    fn prepare(&self, device: &wgpu::Device, queue: &wgpu::Queue, angle: f32) {
+        self.triangle_resources.prepare(device, queue, angle);
+    }
+
+    fn paint<'rp>(&'rp self, render_pass: &mut wgpu::RenderPass<'rp>) {
+        self.triangle_resources.paint(render_pass);
+    }
 }
 
 impl TriangleRenderResources {
